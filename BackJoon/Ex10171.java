84a9216b5fc9e2a3c005c602f6e63a642ffd5e68
DESKTOP-LS42A7O\ip323 <ip323@naver.com>
2021-05-05 22:10:05 +0900

    print a cat figure

---
+++ BackJoon/Ex10171.java
@@ -0,0 +1,14 @@
+package BackJoon;
+
+public class Ex10171 {
+
+	public static void main(String[] args) {
+
+		System.out.println("\\    /\\ ");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}//main
+
+}//class
