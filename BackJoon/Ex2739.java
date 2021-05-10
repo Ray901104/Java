f5ba93425cbfc279facf6e682ac84e8ca0d5eedd
Ray901104 <83386000+Ray901104@users.noreply.github.com>
2021-05-10 12:48:55 +0900

    BackJoon Ex2739
    Print multiplication table taking input from a system.

---
+++ BackJoon/Ex2739.java
@@ -0,0 +1,19 @@
+package BackJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex2739 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<10; i++) {
+			System.out.println(N + " * " + i + " = " + (N*i));
+		}//end for
+	}//main
+
+}//class
