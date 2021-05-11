678a3fe14c1b76c45f7b43eba70262cd498ea5fa
Ray901104 <83386000+Ray901104@users.noreply.github.com>
2021-05-11 10:42:00 +0900

    BackJoon Ex11022
    Test cast : T
    Print sum of A and B like:
    input : 5
    Case #1: 1 + 1 = 2
    Case #2: 2 + 3 = 5
    ...
    Case #5: 5 + 2 = 7

---
+++ BackJoon/Ex11022.java
@@ -0,0 +1,27 @@
+package BackJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Ex11022 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		
+		int T = Integer.parseInt(br.readLine());
+		int A,B;
+		
+		for(int i = 1; i < T+1; i++) {
+			stk = new StringTokenizer(br.readLine(), " ");
+			A = Integer.parseInt(stk.nextToken());
+			B = Integer.parseInt(stk.nextToken());
+			
+			System.out.println("Case #" + i + ": " + A + " + " + B + " = " +(A+B));
+		}//end for
+	}//main
+
+}//class
